086f855012f60463dd14228bd2960a78a0ef5854
Anthony Ruiz <yobakaj@gmail.com>
2023-08-24 19:44:32 -0400

    added sub nav to categories

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,80 @@
-import React, { useState } from 'react';
 import {
   MDBNavbar,
   MDBContainer,
   MDBNavbarBrand,
-  MDBNavbarToggler,
   MDBNavbarItem,
   MDBNavbarLink,
-  MDBCollapse,
   MDBBtn,
   MDBNavbarNav,
   MDBIcon,
   MDBInputGroup
 } from 'mdb-react-ui-kit';
 
+import Nav from 'react-bootstrap/Nav';
+
 export default function App() {
-  const [showNavNoTogglerThird, setShowNavNoTogglerThird] = useState(false);
 
   return (
     <>
       <MDBNavbar expand='lg' dark bgColor='dark'>
         <MDBContainer fluid>
-          <MDBNavbarBrand href='#'>Navbar</MDBNavbarBrand>
-          <MDBCollapse navbar show={showNavNoTogglerThird}>
+          <MDBNavbarBrand href='#'>Orinoco</MDBNavbarBrand>
             <MDBNavbarNav className='mr-auto mb-lg-0'>
+
               <MDBNavbarItem>
                 <MDBNavbarLink active aria-current='page' href='#'>
                   Home
                 </MDBNavbarLink>
               </MDBNavbarItem>
+
               <MDBNavbarItem>
                 <MDBNavbarLink href='#'>Link</MDBNavbarLink>
-              </MDBNavbarItem>
-              <MDBNavbarItem>
-                <MDBNavbarLink disabled href='#' tabIndex={-1} aria-disabled='true'>
-                  Disabled
-                </MDBNavbarLink>
-              </MDBNavbarItem>
+              </MDBNavbarItem>              
 
-              <MDBInputGroup tag="form" className='w-75 h-50'>
+              <MDBInputGroup tag="form" className='ms-4 w-75 h-50'>
                 <input className='form-control' placeholder="Search..." aria-label="Search" type='Search' />
                 <MDBBtn className="w-auto h-auto">
                   <MDBIcon size='lg' fas icon="search" />
                 </MDBBtn>
               </MDBInputGroup>
 
+              <MDBNavbarLink className="ms-auto w-auto h-auto" href='#'>
+                <MDBIcon size='lg' fas icon='user' />
+              </MDBNavbarLink>
+
               <MDBNavbarLink className="ms-auto me-4 w-auto h-auto" href='#'>
                 <MDBIcon size='lg' fas icon='shopping-cart' />
               </MDBNavbarLink>
 
             </MDBNavbarNav>
+        </MDBContainer>
+      </MDBNavbar>
+      <MDBNavbar expand='md' light style={{ backgroundColor: '#D3D3D3' }}>
+        <MDBContainer fluid>
+            <MDBNavbarNav className='d-flex align-items-center justify-content-between'>
+              <MDBNavbarItem>
+                <MDBNavbarLink href='#'>Category 1</MDBNavbarLink>
+              </MDBNavbarItem>
 
-          </MDBCollapse>
+              <MDBNavbarItem>
+                <MDBNavbarLink href='#'>Category 2</MDBNavbarLink>
+              </MDBNavbarItem>
+              <MDBNavbarItem>
+                <MDBNavbarLink href='#'>Category 3</MDBNavbarLink>
+              </MDBNavbarItem>
+
+              <MDBNavbarItem>
+                <MDBNavbarLink href='#'>Category 4</MDBNavbarLink>
+              </MDBNavbarItem>
+              <MDBNavbarItem>
+                <MDBNavbarLink href='#'>Category 5</MDBNavbarLink>
+              </MDBNavbarItem>
+
+              <MDBNavbarItem>
+                <MDBNavbarLink href='#'>Category 6</MDBNavbarLink>
+              </MDBNavbarItem>
+
+            </MDBNavbarNav>
         </MDBContainer>
       </MDBNavbar>
     </>
