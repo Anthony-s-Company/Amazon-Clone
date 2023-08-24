098a2173f3fbd7b9604a4a5209fb1c930dfcf595
Anthony Ruiz <yobakaj@gmail.com>
2023-08-24 18:10:34 -0400

    added and fixed home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,58 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react';
+import {
+  MDBNavbar,
+  MDBContainer,
+  MDBNavbarBrand,
+  MDBNavbarToggler,
+  MDBNavbarItem,
+  MDBNavbarLink,
+  MDBCollapse,
+  MDBBtn,
+  MDBNavbarNav,
+  MDBIcon,
+  MDBInputGroup
+} from 'mdb-react-ui-kit';
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const [showNavNoTogglerThird, setShowNavNoTogglerThird] = useState(false);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      <MDBNavbar expand='lg' dark bgColor='dark'>
+        <MDBContainer fluid>
+          <MDBNavbarBrand href='#'>Navbar</MDBNavbarBrand>
+          <MDBCollapse navbar show={showNavNoTogglerThird}>
+            <MDBNavbarNav className='mr-auto mb-lg-0'>
+              <MDBNavbarItem>
+                <MDBNavbarLink active aria-current='page' href='#'>
+                  Home
+                </MDBNavbarLink>
+              </MDBNavbarItem>
+              <MDBNavbarItem>
+                <MDBNavbarLink href='#'>Link</MDBNavbarLink>
+              </MDBNavbarItem>
+              <MDBNavbarItem>
+                <MDBNavbarLink disabled href='#' tabIndex={-1} aria-disabled='true'>
+                  Disabled
+                </MDBNavbarLink>
+              </MDBNavbarItem>
 
-export default App
+              <MDBInputGroup tag="form" className='w-75 h-50'>
+                <input className='form-control' placeholder="Search..." aria-label="Search" type='Search' />
+                <MDBBtn className="w-auto h-auto">
+                  <MDBIcon size='lg' fas icon="search" />
+                </MDBBtn>
+              </MDBInputGroup>
+
+              <MDBNavbarLink className="ms-auto me-4 w-auto h-auto" href='#'>
+                <MDBIcon size='lg' fas icon='shopping-cart' />
+              </MDBNavbarLink>
+
+            </MDBNavbarNav>
+
+          </MDBCollapse>
+        </MDBContainer>
+      </MDBNavbar>
+    </>
+  );
+}
